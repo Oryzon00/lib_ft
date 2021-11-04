ce5f786592a57a4c6b2c3cbd3026757d873919cb
Adrian <adrian.mael.jung@gmail.com>
2021-11-04 03:39:17 +0100

    plus de fonctions

--- libft.h
+++ libft.h
@@ -21,5 +21,5 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
-
+char	*ft_strnstr(const char *s1, const char *s2, size_t len);
 #endif
