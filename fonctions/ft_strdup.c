890151cda62d64ba80ac4404be2249c785f1c009
Adrian Jung <adrian.mael.jung@gmail.com>
2021-11-23 16:08:08 +0100

    push auto

--- fonctions/ft_strdup.c
+++ fonctions/ft_strdup.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajung <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 11:35:46 by ajung             #+#    #+#             */
-/*   Updated: 2021/11/23 11:35:49 by ajung            ###   ########.fr       */
+/*   Updated: 2021/11/23 15:16:13 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
 
 char	*ft_strdup(const char *s)
 {
