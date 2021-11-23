ef787eb089697a307d1491eb5b36b1f2b8732ac0
Adrian Jung <adrian.mael.jung@gmail.com>
2021-11-23 17:56:18 +0100

    push auto

--- fonctions/ft_lstadd_back.c
+++ fonctions/ft_lstadd_back.c
@@ -3,9 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajung <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 11:36:45 by ajung             #+#    #+#             */
-/*   Updated: 2021/11/23 11:50:45 by ajung            ###   ########.fr       */
+/*   Updated: 2021/11/23 17:55:09 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../libft.h"
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*last;
+
+	if (*alst)
+	{
+		last = *alst;
+		while (last && last->next)
+			last = last->next;
+	last->next = new;
+	}
+	else
+		*alst = new;
+}	
